99d6bbaeddb6f5b488360f55214928fae6a68d87
DuarteMCSilva <duartemcsilva@gmail.com>
2024-01-27 17:46:02 +0100

    Feature: Definition of routes and main resources

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,39 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RouterModule, Routes } from '@angular/router';
+import { AppRoutingModule } from './app-routing.module';
+import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { HomeComponent } from './components/pages/home/home.component';
+import { NotFoundComponent } from './components/pages/not-found/not-found.component';
+import { PortfolioComponent } from './components/pages/portfolio/portfolio.component';
+import { CurriculumComponent } from './components/pages/curriculum/curriculum.component';
+import { ContactsComponent } from './components/pages/contacts/contacts.component';
+
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'contacts', component: ContactsComponent },
+  { path: 'portfolio', component: PortfolioComponent },
+  { path: 'cv', component: CurriculumComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: '**', component: NotFoundComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NotFoundComponent,
+    PortfolioComponent,
+    CurriculumComponent,
+    ContactsComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
