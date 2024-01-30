2a52a12dacd9319b8480a7e3ad6e260822aa5089
DuarteMCSilva <duartemcsilva@gmail.com>
2024-01-30 23:24:30 +0000

    Feature: Portfolio page & Layout adjustments

--- src/app/components/pages/portfolio/portfolio.component.ts
+++ src/app/components/pages/portfolio/portfolio.component.ts
@@ -7,9 +7,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PortfolioComponent implements OnInit {
 
+  projects: ProjectDetails[] = [
+    {
+      name: 'Happy Doggo',
+      image: 'assets/images/happy-doggo.png',
+      description: 'A simple application to search for dog images and discover some breeds.',
+      url: '/happy-doggo',
+      sourceUrl: 'https://github.com/DuarteMCSilva/happy-doggo'
+    },
+
+    {
+      name: 'Happy Doggo',
+      image: 'assets/images/happy-doggo.png',
+      description: 'A simple application to search for dog images and discover some breeds.'
+    },
+    {
+      name: 'Happy Doggo',
+      image: 'assets/images/happy-doggo.png',
+      description: 'A simple application to search for dog images and discover some breeds.'
+    }
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+}
+
+interface ProjectDetails {
+  name: string,
+  image: string,
+  description: string,
+  url?: string,
+  sourceUrl?: string
 }
